2bf0865d12264dcfb7542b1a8b5149344c1c7773
shahajipawar <shahaji.pawar07@gmail.com>
2025-09-06 17:31:30 +0530

    Uncommented 2 test cases from loginTest

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -20,18 +20,18 @@ public class LoginTest extends TestBase{
 				"Shahaji Pawar");
 		
 	}
-//
-//	@Test(description = "Verifies with the valid user is able to login into the application", groups = { "e2e",
-//			"Sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider")
-//	public void loginCSVTest(User user) {
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUsername(),
-//				"Shahaji Pawar");
-//	}
-//
-//	@Test(description = "Verifies with the valid user is able to login into the application", groups = { "e2e",
-//			"Sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
-//	public void loginExcelTest(User user) {
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUsername(),
-//				"Shahaji Pawar");
-//	}
+
+	@Test(description = "Verifies with the valid user is able to login into the application", groups = { "e2e",
+			"Sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider")
+	public void loginCSVTest(User user) {
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUsername(),
+				"Shahaji Pawar");
+	}
+
+	@Test(description = "Verifies with the valid user is able to login into the application", groups = { "e2e",
+			"Sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
+	public void loginExcelTest(User user) {
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUsername(),
+				"Shahaji Pawar");
+	}
 }
